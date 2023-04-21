ec23a88d6036a98630be5f4d4202a20da147192b
lee seung ah <lee88067@gmail.com>
2023-04-21 18:21:40 +0900

    Login Modal

---
+++ src/routes/NotFound.tsx
@@ -0,0 +1,12 @@
+import { Button, Heading, Text, VStack } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
+
+export default function Notfound(){
+    return <VStack bg="gray.100" justifyContent={"center"} minH="100vh">
+        <Heading>Page not found.</Heading>
+        <Text>It seems thant you're lost.</Text>
+        <Link to="/">
+            <Button colorScheme={"twitter"} variant={"link"}>Go home &rarr;</Button>
+        </Link>
+    </VStack>
+}
